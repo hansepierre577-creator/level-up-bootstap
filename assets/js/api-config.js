ca3593b959a8ff515da46e449fc975c7a3a44927
Hanse Pierre <hansepierre577@gmail.com>
2026-09-18 11:52:57 -0400

    Use Level-Up Cloudflare Worker URL

--- assets/js/api-config.js
+++ assets/js/api-config.js
@@ -4,7 +4,7 @@
   // Set LEVELUP_API_BASE_URL in the page before this file for a separately
   // deployed Worker (for example, https://level-up-api.<account>.workers.dev).
   const defaultBase = window.LEVELUP_API_BASE_URL
-    || (isLocal ? 'http://localhost:8787' : 'https://level-up-api.hansepierre577.workers.dev');
+    || (isLocal ? 'http://localhost:8787' : 'https://level-up-bootstap.hansepierre577.workers.dev');
   const stripeKey = 'levelup_stripe_public_key';
   const defaultStripePublicKey = window.LEVELUP_STRIPE_PUBLIC_KEY || 'pk_test_replace_me';
 
